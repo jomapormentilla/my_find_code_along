59ad9d2cf9356f9ec9ba83f4a23eb8e49010ae70
jomapormentilla <jomapormentilla@gmail.com>
2020-10-21 16:49:44 -0400

    commit tree 9397d81c34c8a29af2c2b32d7cb3b47724cea1b0

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,9 @@
 require 'pry'
 
-def my_find(collection)
-
+def my_find( array )
+    count = 0
+    while count < array.length
+        return array[count] if yield( array[count] )
+        count += 1
+    end
 end
